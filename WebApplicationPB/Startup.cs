fa37b63485d700747c8dd9e42f60c9c1da7dcc2e
pim <trial.pim@outlook.com>
2016-08-26 00:56:19 +0000

    test 123

---
+++ WebApplicationPB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationPB.Startup))]
+namespace WebApplicationPB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
